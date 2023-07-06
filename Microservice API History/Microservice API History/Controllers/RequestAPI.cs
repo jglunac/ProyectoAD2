8ba40cc7f309e3182559f70602eb7e38888c0ad2
jglunac <59965122+jglunac@users.noreply.github.com>
2023-07-05 23:27:10 -0600

    Finished

--- Microservice API History/Microservice API History/Controllers/RequestAPI.cs
+++ Microservice API History/Microservice API History/Controllers/RequestAPI.cs
@@ -44,133 +44,120 @@ namespace Microservice_API_History.Controllers
             }
         }
 
-//        var database = dbClient.GetDatabase("Resultados");
-//        List<BsonDocument> Mesas = new List<BsonDocument>();
-//        var collection = database.GetCollection<BsonDocument>("Resultados2023");
-//        var documents = collection.Find(new BsonDocument()).ToList();
-//            if (documents.Count != 0)
-//            {
-//                Mesa[] MesaObjeto = new Mesa[documents.Count];
-//                foreach (var item in documents)
-//                {
-//                    Mesas.Add(item);
-//                }
-//                for (int k = 0;  k<Mesas.Count; k++)
-//                {
-//                    MesaObjeto[k] = BsonSerializer.Deserialize<Mesa>(Mesas[k]);
-//                }
-//string json = JsonSerializer.Serialize(MesaObjeto);
-//return Ok(json);
-//            }
-//            else
-//{
-//    return NotFound();
-//}
-            //switch (period)
-            //{
-            //    case 0:
-                    
-            //    case 1:
-            //        var collection1 = database.GetCollection<BsonDocument>("Resultados2019");
-            //        var documents1 = collection1.Find(new BsonDocument()).ToList();
-            //        if (documents1.Count != 0)
-            //        {
-            //            Mesa[] MesaObjeto = new Mesa[documents1.Count];
-            //            int i = 0;
-            //            foreach (var item in documents1)
-            //            {
-            //                MesaObjeto[i] = BsonSerializer.Deserialize<Mesa>(item);
-            //                i++;
-            //            }
+        [HttpGet]
+        [Route("request2019")]
 
-            //            string json = JsonSerializer.Serialize(MesaObjeto);
-            //            return Ok(json);
-            //        }
-            //        else
-            //        {
-            //            return NotFound();
-            //        }
-            //    case 2:
-            //        var collection2 = database.GetCollection<BsonDocument>("Resultados2015");
-            //        var documents2 = collection2.Find(new BsonDocument()).ToList();
-            //        if (documents2.Count != 0)
-            //        {
-            //            Mesa[] MesaObjeto = new Mesa[documents2.Count];
-            //            int i = 0;
-            //            foreach (var item in documents2)
-            //            {
-            //                MesaObjeto[i] = BsonSerializer.Deserialize<Mesa>(item);
-            //                i++;
-            //            }
+        public IActionResult History2019(int period)
+        {
+            MongoClient dbClient = new MongoClient(connectionUri);
 
-            //            string json = JsonSerializer.Serialize(MesaObjeto);
-            //            return Ok(json);
-            //        }
-            //        else
-            //        {
-            //            return NotFound();
-            //        }
-            //    case 3:
-            //        var collection3 = database.GetCollection<BsonDocument>("Resultados2011");
-            //        var documents3 = collection3.Find(new BsonDocument()).ToList();
-            //        if (documents3.Count != 0)
-            //        {
-            //            Mesa[] MesaObjeto = new Mesa[documents3.Count];
-            //            int i = 0;
-            //            foreach (var item in documents3)
-            //            {
-            //                MesaObjeto[i] = BsonSerializer.Deserialize<Mesa>(item);
-            //                i++;
-            //            }
+            var database = dbClient.GetDatabase("Resultados");
+            var collection = database.GetCollection<BsonDocument>("Resultados2019");
+            var document = collection.Find(new BsonDocument()).ToList();
+            var ListaMesas = new List<Mesa>();
+            if (document.Count != 0)
+            {
+                foreach (var item in document)
+                {
+                    ListaMesas.Add(BsonSerializer.Deserialize<Mesa>(item));
+                }
+                Mesas mesas = new Mesas();
+                mesas.mesas = ListaMesas;
+                mesas.count = ListaMesas.Count;
+                string json = JsonSerializer.Serialize(mesas);
+                return Ok(json);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
 
-            //            string json = JsonSerializer.Serialize(MesaObjeto);
-            //            return Ok(json);
-            //        }
-            //        else
-            //        {
-            //            return NotFound();
-            //        }
-            //    case 4:
-            //        var collection4 = database.GetCollection<BsonDocument>("Resultados2007");
-            //        var documents4 = collection4.Find(new BsonDocument()).ToList();
-            //        if (documents4.Count != 0)
-            //        {
-            //            Mesa[] MesaObjeto = new Mesa[documents4.Count];
-            //            int i = 0;
-            //            foreach (var item in documents4)
-            //            {
-            //                MesaObjeto[i] = BsonSerializer.Deserialize<Mesa>(item);
-            //                i++;
-            //            }
+        [HttpGet]
+        [Route("request2023")]
 
-            //            string json = JsonSerializer.Serialize(MesaObjeto);
-            //            return Ok(json);
-            //        }
-            //        else
-            //        {
-            //            return NotFound();
-            //        }
-            //    default:
-            //        var collection5 = database.GetCollection<BsonDocument>("Resultados2023");
-            //        var documents5 = collection5.Find(new BsonDocument()).ToList();
-            //        if (documents5.Count != 0)
-            //        {
-            //            Mesa[] MesaObjeto = new Mesa[documents5.Count];
-            //            int i = 0;
-            //            foreach (var item in documents5)
-            //            {
-            //                MesaObjeto[i] = BsonSerializer.Deserialize<Mesa>(item);
-            //                i++;
-            //            }
+        public IActionResult History2015(int period)
+        {
+            MongoClient dbClient = new MongoClient(connectionUri);
 
-            //            string json = JsonSerializer.Serialize(MesaObjeto);
-            //            return Ok(json);
-            //        }
-            //        else
-            //        {
-            //            return NotFound();
-            //        }
-            //}
-        //}
+            var database = dbClient.GetDatabase("Resultados");
+            var collection = database.GetCollection<BsonDocument>("Resultados2015");
+            var document = collection.Find(new BsonDocument()).ToList();
+            var ListaMesas = new List<Mesa>();
+            if (document.Count != 0)
+            {
+                foreach (var item in document)
+                {
+                    ListaMesas.Add(BsonSerializer.Deserialize<Mesa>(item));
+                }
+                Mesas mesas = new Mesas();
+                mesas.mesas = ListaMesas;
+                mesas.count = ListaMesas.Count;
+                string json = JsonSerializer.Serialize(mesas);
+                return Ok(json);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpGet]
+        [Route("request2023")]
+
+        public IActionResult History2011(int period)
+        {
+            MongoClient dbClient = new MongoClient(connectionUri);
+
+            var database = dbClient.GetDatabase("Resultados");
+            var collection = database.GetCollection<BsonDocument>("Resultados2011");
+            var document = collection.Find(new BsonDocument()).ToList();
+            var ListaMesas = new List<Mesa>();
+            if (document.Count != 0)
+            {
+                foreach (var item in document)
+                {
+                    ListaMesas.Add(BsonSerializer.Deserialize<Mesa>(item));
+                }
+                Mesas mesas = new Mesas();
+                mesas.mesas = ListaMesas;
+                mesas.count = ListaMesas.Count;
+                string json = JsonSerializer.Serialize(mesas);
+                return Ok(json);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpGet]
+        [Route("request2023")]
+
+        public IActionResult History2007(int period)
+        {
+            MongoClient dbClient = new MongoClient(connectionUri);
+
+            var database = dbClient.GetDatabase("Resultados");
+            var collection = database.GetCollection<BsonDocument>("Resultados2007");
+            var document = collection.Find(new BsonDocument()).ToList();
+            var ListaMesas = new List<Mesa>();
+            if (document.Count != 0)
+            {
+                foreach (var item in document)
+                {
+                    ListaMesas.Add(BsonSerializer.Deserialize<Mesa>(item));
+                }
+                Mesas mesas = new Mesas();
+                mesas.mesas = ListaMesas;
+                mesas.count = ListaMesas.Count;
+                string json = JsonSerializer.Serialize(mesas);
+                return Ok(json);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
     }
 }
--- SEA System/SEA System/Controllers/HistoryController.cs
+++ SEA System/SEA System/Controllers/HistoryController.cs
@@ -23,15 +23,76 @@ namespace SEA_System.Controllers
                 client.DefaultRequestHeaders.Clear();
                 
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                HttpResponseMessage Res = await client.GetAsync("api/request2023");
-                if (Res.IsSuccessStatusCode)
+                switch (id)
                 {
-                    string Respuesta = Res.Content.ReadAsStringAsync().Result;
-                    string str = JToken.Parse(Respuesta).ToString();
+                    case 0:
+                        HttpResponseMessage Res = await client.GetAsync("api/request2023");
+                        if (Res.IsSuccessStatusCode)
+                        {
+                            string Respuesta = Res.Content.ReadAsStringAsync().Result;
+                            string str = JToken.Parse(Respuesta).ToString();
 
-                    var MesasContadas1 = JsonSerializer.Deserialize<Mesas>(str);
-                    ViewBag.Mesas = MesasContadas1;
+                            var MesasContadas1 = JsonSerializer.Deserialize<Mesas>(str);
+                            ViewBag.selected = MesasContadas1;
+                        }
+                        break;
+                    case 1:
+                        HttpResponseMessage Res1 = await client.GetAsync("api/request2019");
+                        if (Res1.IsSuccessStatusCode)
+                        {
+                            string Respuesta = Res1.Content.ReadAsStringAsync().Result;
+                            string str = JToken.Parse(Respuesta).ToString();
+
+                            var MesasContadas1 = JsonSerializer.Deserialize<Mesas>(str);
+                            ViewBag.selected = MesasContadas1;
+                        }
+                        break;
+                    case 2:
+                        HttpResponseMessage Res2 = await client.GetAsync("api/request2015");
+                        if (Res2.IsSuccessStatusCode)
+                        {
+                            string Respuesta = Res2.Content.ReadAsStringAsync().Result;
+                            string str = JToken.Parse(Respuesta).ToString();
+
+                            var MesasContadas1 = JsonSerializer.Deserialize<Mesas>(str);
+                            ViewBag.selected = MesasContadas1;
+                        }
+                        break;
+                    case 3:
+                        HttpResponseMessage Res3 = await client.GetAsync("api/request2011");
+                        if (Res3.IsSuccessStatusCode)
+                        {
+                            string Respuesta = Res3.Content.ReadAsStringAsync().Result;
+                            string str = JToken.Parse(Respuesta).ToString();
+
+                            var MesasContadas1 = JsonSerializer.Deserialize<Mesas>(str);
+                            ViewBag.selected = MesasContadas1;
+                        }
+                        break;
+                    case 4:
+                        HttpResponseMessage Res4 = await client.GetAsync("api/request2007");
+                        if (Res4.IsSuccessStatusCode)
+                        {
+                            string Respuesta = Res4.Content.ReadAsStringAsync().Result;
+                            string str = JToken.Parse(Respuesta).ToString();
+
+                            var MesasContadas1 = JsonSerializer.Deserialize<Mesas>(str);
+                            ViewBag.selected = MesasContadas1;
+                        }
+                        break;
+                    case 5:
+                        HttpResponseMessage Res5 = await client.GetAsync("api/request2023");
+                        if (Res5.IsSuccessStatusCode)
+                        {
+                            string Respuesta = Res5.Content.ReadAsStringAsync().Result;
+                            string str = JToken.Parse(Respuesta).ToString();
+
+                            var MesasContadas1 = JsonSerializer.Deserialize<Mesas>(str);
+                            ViewBag.selected = MesasContadas1;
+                        }
+                        break;
                 }
+                
 
                 return View("MesasContadas");
             }
